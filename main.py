092e14470b1a145934cc2491dd0262c0b9aff840
NathanNuckels <48299408+NathanNuckels@users.noreply.github.com>
2022-01-03 16:18:35 -0600

    Finished Most of the UI

--- main.py
+++ main.py
@@ -13,17 +13,6 @@ button1.grid(row=1,column=0,sticky=NSEW)
 button2.grid(row=1,column=1,sticky=NSEW)
 button3.grid(row=1,column=2,sticky=NSEW)
 button4.grid(row=1,column=3,sticky=NSEW)
-
-canvas.create_rectangle(0,0,1280,720,fill="black")
-canvas.create_text(100,100,anchor=NW,text="Hey hey! Please don't put this in fullscreen.\nIt kinda looks weird",fill="white",font=("Helvetica",24))
-tk.title("Glass Closet - Objects: 2, Clock: 1337")
-tick=500
-while tick!=0:
-	tk.update()
-	tk.title("Glass Closet - Objects: 2, Clock: "+str(tick))
-	tick-=1
-	time.sleep(0.01)
-canvas.delete("all")
 canvas.create_rectangle(0,0,1280,720,fill="black")
 tick=0
 tk.title("Glass Closet")
@@ -51,20 +40,50 @@ while tick!=0:
 	tk.update()
 	time.sleep(0.01)
 	canvas.delete("all")
+ofset=80
+canvas.create_rectangle(0,0,1280,720,fill="black")
 tt=[
-	canvas.create_text(0,360,font=("Helvetica",36),text="B"),
-	canvas.create_text(0,360,font=("Helvetica",36),text="r"),
-	canvas.create_text(0,360,font=("Helvetica",36),text="e"),
-	canvas.create_text(0,360,font=("Helvetica",36),text="a"),
-	canvas.create_text(0,360,font=("Helvetica",36),text="k"),
-	canvas.create_text(0,360,font=("Helvetica",36),text="t"),
-	canvas.create_text(0,360,font=("Helvetica",36),text="h"),
-	canvas.create_text(0,360,font=("Helvetica",36),text="r"),
-	canvas.create_text(0,360,font=("Helvetica",36),text="o"),
-	canvas.create_text(0,360,font=("Helvetica",36),text="u"),
-	canvas.create_text(0,360,font=("Helvetica",36),text="g"),
-	canvas.create_text(0,360,font=("Helvetica",36),text="h")]
+	canvas.create_text(ofset,1000,font=("Helvetica",24),text="B",fill="white"),
+	canvas.create_text(ofset+100,1000,font=("Helvetica",24),text="r",fill="white"),
+	canvas.create_text(ofset+200,1000,font=("Helvetica",24),text="e",fill="white"),
+	canvas.create_text(ofset+300,1000,font=("Helvetica",24),text="a",fill="white"),
+	canvas.create_text(ofset+400,1000,font=("Helvetica",24),text="k",fill="white"),
+	canvas.create_text(ofset+500,1000,font=("Helvetica",24),text="t",fill="white"),
+	canvas.create_text(ofset+600,1000,font=("Helvetica",24),text="h",fill="white"),
+	canvas.create_text(ofset+700,1000,font=("Helvetica",24),text="r",fill="white"),
+	canvas.create_text(ofset+800,1000,font=("Helvetica",24),text="o",fill="white"),
+	canvas.create_text(ofset+900,1000,font=("Helvetica",24),text="u",fill="white"),
+	canvas.create_text(ofset+1000,1000,font=("Helvetica",24),text="g",fill="white"),
+	canvas.create_text(ofset+1100,1000,font=("Helvetica",24),text="h",fill="white")]
 
+for z in range(11):
+	for i in range(40):
+		canvas.move(tt[z],0,-16)
+		tk.update()
+		time.sleep(0.01)
+
+for i in range(128):
+	canvas.move(tt[11],0,-5)
+	tk.update()
+	time.sleep(0.01)
+
+for z in range(100):
+	for i in range(12):
+		canvas.move(tt[i],0,-5*(12-i))
+	tk.update()
+	time.sleep(0.01)
+
+for i in range(64):
+	col=i*4
+	if col==256:
+		col=255
+	col=hex(col)[2:]
+	if len(col)==1:
+		col="0"+col
+	col="#"+col+col+col
+	canvas.create_rectangle(0,0,1280,720,fill=col)
+	tk.update()
+	time.sleep(0.01)
 ticks=0
 while 1:
 	ticks+=1
